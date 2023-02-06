28ba9e90a2cb812e985d68c382a6b314b45cd822
ahmetkotan <ahmtkotan@gmail.com>
2023-02-06 21:08:42 +0300

    add static directory

--- trquake/settings/base.py
+++ trquake/settings/base.py
@@ -130,6 +130,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_ROOT = BASE_DIR / "statics" / "root"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
