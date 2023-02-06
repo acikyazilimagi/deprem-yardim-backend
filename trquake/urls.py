a1a7538a45ad52d207213c469591f6d6ddb20a8a
ahmetkotan <ahmtkotan@gmail.com>
2023-02-06 22:06:11 +0300

    add static url

--- trquake/urls.py
+++ trquake/urls.py
@@ -15,5 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
 
-urlpatterns = [path("admin/", admin.site.urls), path("core/", include("core.urls"))]
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("core/", include("core.urls")),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
