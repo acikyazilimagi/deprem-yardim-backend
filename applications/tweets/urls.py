4185e68998dfe51f3bd0329c5dd52d913f583ff1
ahmetkotan <ahmtkotan@gmail.com>
2023-02-07 16:53:31 +0300

    add areas count view

--- applications/tweets/urls.py
+++ applications/tweets/urls.py
@@ -1,9 +1,10 @@
 from rest_framework.routers import DefaultRouter
-from tweets.views import LocationViewSet, AreaViewSet, CityByCityCountView
+from tweets.views import LocationViewSet, AreaViewSet, AreasCountViewSet, CityByCityCountView
 from django.urls import path
 
 router = DefaultRouter(trailing_slash=False)
 router.register("locations", LocationViewSet, basename="list-locations")
+router.register("areas/count", AreasCountViewSet, basename="count-area")
 router.register("areas", AreaViewSet, basename="list-area")
 
 urlpatterns = [path("cities", CityByCityCountView.as_view())] + router.urls
